ff7dcf89fc7705fe5f28b035143fa53bde5ce059
Muhammad Atif <muhammadatifkhan.99@gmail.com>
2025-09-08 23:47:13 +0500

    finding how many times an array is rotated

---
+++ FindOutHowManyTimesAnArrayisRotated/code.cpp
@@ -0,0 +1,122 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+//Find the minimum in rotated Sorted Array 
+
+//Brute Force Approach 
+
+int findingMinimumSorted(vector<int> &arr){
+    int n = arr.size();
+
+    int mini = INT_MAX;
+    for(int i{0};i<n;i++){
+        if(arr[i] < mini){
+            mini = arr[i];
+        }
+    }
+    return mini;
+}
+
+//Best and optimal approaches with BS
+
+int findingMinimuminSorted(vector<int> &arr){
+    int n = arr.size();
+
+    int low = 0;
+    int high = n-1;
+    int ans = INT_MAX;
+
+    while(low <= high){
+        int mid = low+((high-low)/2);
+        //finding the sorted part of the array 
+        if(arr[low] <= arr[mid]){
+            ans = min(ans,arr[low]);
+            low = mid+1;
+        } else {
+            ans = min(ans,arr[mid]);
+            high = mid-1;
+        }
+    }
+    return ans;
+}
+
+//minor optimization if we find the sorted part of the array
+int findingMinimuminSortedOpti(vector<int> &arr){
+    int n = arr.size();
+
+    int low = 0;
+    int high = n-1;
+    int ans = INT_MAX;
+
+    while(low <= high){
+        int mid = low+((high-low)/2);
+        //finding the sorted part of the array 
+        if(arr[low] <= arr[high]){
+            ans = min(ans,arr[low]);
+            break;
+        }
+        if(arr[low] <= arr[mid]){
+            ans = min(ans,arr[low]);
+            low = mid+1;
+        } else {
+            ans = min(ans,arr[mid]);
+            high = mid-1;
+        }
+    }
+    return ans;
+}
+
+int findingMinimuminSortedOptir(vector<int> &arr){
+    int n = arr.size();
+
+    int low = 0;
+    int high = n-1;
+    int ans = INT_MAX;
+    int index = -1;
+
+    while(low <= high){
+        int mid = low+((high-low)/2);
+        //finding the sorted part of the array 
+        if(arr[low] <= arr[high]){
+            if(arr[low] < ans){
+                ans = arr[low];
+                index = low;
+            }
+            break;
+        }
+        if(arr[low] <= arr[mid]){
+            if(arr[low] < ans){
+                ans = arr[low];
+                index = low;
+            }
+            low = mid+1;
+        } else {
+            if(arr[mid] < ans){
+                ans = arr[mid];
+                index = mid;
+            }
+            high = mid-1;
+        }
+    }
+    return index;
+}
+
+
+int main(){
+
+    //###################################################################
+    cout<<"Play ground is up and running, just Code !!!" << endl;
+    cout << endl;
+    //###################################################################
+
+    vector<int> ans = {3,4,5,1,2};
+    cout << findingMinimumSorted(ans);
+    cout << endl;
+    cout << findingMinimuminSorted(ans);
+    cout << endl;
+    cout << findingMinimuminSortedOptir(ans);
+
+
+    return 0;
+}
