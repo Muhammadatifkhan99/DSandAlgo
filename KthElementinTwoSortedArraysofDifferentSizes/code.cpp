61bfc334ef2c3b8141f0f29470e86df5f6b97c06
Muhammad Atif <muhammadatifkhan.99@gmail.com>
2025-10-05 17:08:46 +0500

    finding the kth element in two sorted arrays

---
+++ KthElementinTwoSortedArraysofDifferentSizes/code.cpp
@@ -0,0 +1,67 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+//kth element of two sorted arrays of different sizes
+
+long double median(vector<int> &a, vector<int> &b,int k){
+    int n1 = a.size();
+    int n2 = b.size();
+
+    if(n1>n2) return median(b,a,k); //make sure the shorter array is used for BS
+    int low = max(0,k-n2);
+    int high = min(k,n1);
+    int left = k;
+    int n= n1+n2;
+    while(low <= high){
+        int mid1 = (low+high)/2;
+        int mid2 = left-mid1;
+        int l1 = INT_MIN; 
+        int l2 = INT_MIN;
+        int r1 = INT_MAX;
+        int r2 = INT_MAX;
+        if(mid1 < n1) r1 = a[mid1];
+        if(mid2 < n2) r2 = b[mid2];
+        if(mid1-1 >= 0) l1 = a[mid1 -1];
+        if(mid2 -1 >= 0) l2 = b[mid2-1];
+        if(l1 <= r2 && l2<=r1) {
+            return max(l1,l2);
+        } else if(l1>r2) high = mid1-1;
+        else low=mid1+1;
+    }
+    return 0;
+}
+
+int linearSearch(int arr[],int target,int n){
+        for(int i{0};i<n;i++){
+        if(arr[i] == target){
+            return i;
+            break;
+        }
+    }
+    return -1;
+}
+
+
+
+
+int main(){
+
+    //###################################################################
+    cout<<"Play ground is up" << endl;
+    cout << endl;
+    //###################################################################
+
+    // vector<int> ans = {2,3,6,7,9};
+    // vector<int> ans1 = {1,4,8,10};
+    // cout << median(ans,ans1,4) << endl;
+
+    int arr[] = {2,3,56,34,12,4,5,2};
+    int target = 4;
+    int n = sizeof(arr);
+    cout << n/4 << endl;
+
+    cout << linearSearch(arr,target,n/4) << endl;
+    
+
+    return 0;
+}
