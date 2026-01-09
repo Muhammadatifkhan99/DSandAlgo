a09a9d089952329d4fdeab0fee4ce220981d2bed
Atif A. Khan <muhammadatifkhan.99@gmail.com>
2026-01-09 12:49:34 +0500

    graphs data structure introduction and some theoritcial work

---
+++ GraphsIntroduction/code.cpp
@@ -0,0 +1,61 @@
+#include<iostream>
+#include<vector>
+#include <climits>
+
+
+using namespace std;
+
+
+int main(){
+
+    int n, m;
+    cin>>m>>n;
+
+    int adj[n+1][n+1];
+
+// initialize matrix with 0
+    for(int i = 1; i <= n; i++){
+        for(int j = 1; j <= n; j++){
+            adj[i][j] = 0;
+        }
+    }
+
+    //adjacency matrix way
+    for(int i{0};i<m;i++){
+        int u,v;
+        cin>>u>>v;
+        adj[u][v] = 1;
+        adj[v][u] = 1;
+    }
+
+
+    //list way to store these values...........
+
+    vector<vector<int>> adjv(n+1);
+    for(int i{0};i<m;i++){
+        int u,v;
+        cin>>u>>v;
+        adjv[u].push_back(v);
+        adjv[v].push_back(u);
+    }
+
+    cout << "using adjacency method" << endl;
+    for(int i = 1; i <= n; i++){
+        for(int j = 1; j <= n; j++){
+            cout << adj[i][j] << " ";
+        }
+    cout << endl;
+    }
+
+
+    cout<<"using list method " << endl;
+    for(int i = 1; i <= n; i++){
+        for(int j = 1; j <= n; j++){
+            cout << adjv[i][j] << " ";
+        }
+    cout << endl;
+}
+
+
+    return 0;
+}
